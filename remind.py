5dfdc4d392ec6aa16a9a104398050c64aef9eea1
Léa Heurtaux <149768417+leaahrtx@users.noreply.github.com>
2025-02-26 13:24:20 +0000

    modif

--- remind.py
+++ remind.py
@@ -1,3 +1,16 @@
+import discord
+from discord.ext import commands, tasks
+import asyncio
+import datetime
+import json
+import os
+from typing import Dict, List, Optional
+
+# Bot configuration
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
 # Data storage
 REMINDERS_FILE = 'reminders.json'
 
